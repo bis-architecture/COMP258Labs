f83fed11cd15b3580e7de63ecb70e20d532cd7b3
bis-architecture <323689011+bis-architecture@users.noreply.github.com>
2026-09-01 11:12:30 -0600

    Initial commit

---
+++ src/comp258labs/COMP258Labs.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package comp258labs;
+
+/**
+ *
+ * @author antoi
+ */
+public class COMP258Labs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to COMP 258");
+
+    }
+
+}
